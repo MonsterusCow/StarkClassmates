04cd2c67e6dff17860d8c0322dbe89caacd34db8
MonsterusCow <ryan.stark26@d155.org>
2024-10-01 07:28:07 -0500

    Initial Commit

---
+++ StarkClassmates/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StarkClassmates
+//
+//  Created by RYAN STARK on 10/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ StarkClassmatesTests/StarkClassmatesTests.swift
@@ -0,0 +1,17 @@
+//
+//  StarkClassmatesTests.swift
+//  StarkClassmatesTests
+//
+//  Created by RYAN STARK on 10/1/24.
+//
+
+import Testing
+@testable import StarkClassmates
+
+struct StarkClassmatesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
