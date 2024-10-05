3aa8d17f13949ccc4e4a469e38dd50480054d14d
Ryan <ryan@starkfamily.org>
2024-10-05 12:26:18 -0500

    finished

--- StarkClassmates/ViewController.swift
+++ StarkClassmates/ViewController.swift
@@ -20,15 +20,15 @@ class ViewController: UIViewController {
     var daniel = ClassMate(Name: "Daniel", Nick: "Bananiael", Age: 102, FavoriteColor: "red", Grade: 68.99, Gender: gender.boy)
     var micheal = ClassMate(Name: "Micheal", Nick: "Micheal Micheal Tricical", Age: 16, FavoriteColor: "Blue", Grade: 127, Gender: gender.boy)
     var googan = ClassMate(Name: "Googan", Nick: "Googball Googan", Age: 16, FavoriteColor: "Green", Grade: 210, Gender: gender.boy)
-    var sean
-    var ryan
-    var cam
-    var brennen
-    var eva = no pickles noftz
-    var ava
-    var justin
-    var jaiden
-    var john = jimmy
+    var sean = ClassMate(Name: "Sean", Nick: "Boat Gone Sean", Age: 16, FavoriteColor: "Light Blue", Grade: 12.3, Gender: gender.boy)
+    var ryan = ClassMate(Name: "Ryan", Nick: "Rainbow Ryan", Age: 16, FavoriteColor: "Silver", Grade: 192, Gender: gender.boy)
+    var cam = ClassMate(Name: "Cam", Nick: "Chef Cam", Age: 15, FavoriteColor: "Tan", Grade: 99.99, Gender: gender.boy)
+    var brennen = ClassMate(Name: "Brennen", Nick: "Breakin Brennen", Age: 15, FavoriteColor: "Sea Green", Grade: 45.3, Gender: gender.boy)
+    var eva = ClassMate(Name: "Eva", Nick: "No Pickles Noftz", Age: 17, FavoriteColor: "Cyan", Grade: 256, Gender: gender.girl)
+    var ava = ClassMate(Name: "Ava", Nick: "Abba Ava", Age: 17, FavoriteColor: "Teal", Grade: 1, Gender: gender.girl)
+    var justin = ClassMate(Name: "Justin", Nick: "Trustin Justin", Age: 17, FavoriteColor: "Hot Pink", Grade: 97.3, Gender: gender.boy)
+    var jaiden = ClassMate(Name: "Jaiden", Nick: "Sleepy Saurer", Age: 17, FavoriteColor: "Medium Purple", Grade: 10, Gender: gender.boy)
+    var john = ClassMate(Name: "John", Nick: "Jimmy John", Age: 17, FavoriteColor: "Dark Green", Grade: 67, Gender: gender.boy)
     
 
     override func viewDidLoad() {
@@ -38,6 +38,17 @@ class ViewController: UIViewController {
         thing.students.append(mattew)
         thing.students.append(peter)
         thing.students.append(daniel)
+        thing.students.append(micheal)
+        thing.students.append(googan)
+        thing.students.append(sean)
+        thing.students.append(ryan)
+        thing.students.append(cam)
+        thing.students.append(brennen)
+        thing.students.append(eva)
+        thing.students.append(ava)
+        thing.students.append(justin)
+        thing.students.append(jaiden)
+        thing.students.append(john)
         labelbutton.layer.cornerRadius = 10
         labelbutton.layer.masksToBounds = true
         
